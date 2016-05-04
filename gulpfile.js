fff45563db4977c652b5792d6314930dd5f4a4d7
cherishhsieh <xieqin1991@gmail.com>
2016-05-04 10:42:14 +0800

    add gulp-svg-sprites

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     concat  = require('gulp-concat'),
+    svgSprite = require('gulp-svg-sprites');
 	spritesmith = require('gulp.spritesmith');
 
+//less文件编译合并
 gulp.task('css', function () {
     var cssSrc = './assets/less/*.less',
         cssDst = './assets/css';
@@ -19,6 +21,7 @@ gulp.task('css', function () {
         .pipe(gulp.dest(cssDst))
 });
 
+//制作雪碧图及生成对应css文件
 gulp.task('sprites', function() {
     var data =
         gulp.src('./assets/images/icons/*.*')
@@ -31,6 +34,15 @@ gulp.task('sprites', function() {
     data.css.pipe(gulp.dest('./assets/css'));
 })
 
+//制作svg雪碧图
+gulp.task('svg-sprites', function() {
+    return gulp.src('./assets/images/svg/*.svg')
+            .pipe(svgSprite())
+            .pipe(rename({ suffix: '.svg' }))
+            .pipe(gulp.dest('./assets'))
+})
+
+//监控less
 gulp.task('watch', function() {
     gulp.watch('./assets/less/*.*', ['css'])
 })
