ed3edb43fb1668de4a8042a758b86d320674a327
cherishhsieh <xieqin1991@gmail.com>
2016-06-20 14:07:03 +0800

    add hints

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 $(function() {
+  //绑定fastclick
   FastClick.attach(document.body);
 
+  /**
+   * [actionList action对象]
+   * @type {Object}
+   */
   var actionList = {
     //demo
     'test': function(self) {
@@ -9,6 +14,9 @@ $(function() {
   }
   var $body = $(document)
 
+  /**
+   * 事件委托的click事件管理
+   */
   $body.on('click', '[data-action]', function () {
     var actionName = $(this).data('action')
     var action = actionList[actionName]
